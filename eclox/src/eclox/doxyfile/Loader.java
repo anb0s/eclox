5eaccc62e4d7e228ba72da0e356396f8acaf3a15
gbrocker <guillaume.brocker@gmail.com>
2004-02-16 21:56:34 +0000

    [code] Added a eclox.doxyfile.Tokenizer for doxyfile parsing. Deactivated the loader. Added a function in the eclox.doxyfile.node.Node interface to allow dynamic text addition to the node.

--- eclox/src/eclox/doxyfile/Loader.java
+++ eclox/src/eclox/doxyfile/Loader.java
@@ -21,11 +21,12 @@
 
 package eclox.doxyfile;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 
 import eclox.doxyfile.node.Comment;
+import eclox.doxyfile.node.Doxyfile;
+import eclox.doxyfile.node.Group;
 import eclox.doxyfile.node.Node;
 import eclox.doxyfile.node.Section;
 import eclox.doxyfile.node.Tag;
@@ -36,87 +37,10 @@ import eclox.doxyfile.node.Tag;
  * @author gbrocker
  */
 public class Loader {
-	
-	/**
-	 * @brief	Defines the input reader.	
-	 */
-	private class Reader
-	{
-		/**
-		 * The input stream used for the settings construction.
-		 */
-		private BufferedReader m_in;
-		
-		/**
-		 * @brief	A line of text that has been previewed..
-		 */
-		private String m_preview = null;
-		
-		/**
-		 * @brief	Constructor.
-		 * 
-		 * @param	input	The The input stream to read.
-		 */
-		public Reader( InputStream input )
-		{
-			m_in = new java.io.BufferedReader( new java.io.InputStreamReader( input ) );	
-		}
-		
-		/**
-		 * @brief	Retrieve the next line of text..
-		 * 
-		 * @return	A string containing the next line of or null if the end of file
-		 * 			has been reached.
-		 */
-		public String getLine() throws java.io.IOException {
-			String	result;
-			
-			if( m_preview == null ) {
-				result = readLine();
-			}
-			else {
-				result = m_preview;
-				m_preview = null;
-			}
-			return result;
-		}
-		
-		/**
-		 * @brief	Preview the next line of text. The line of text is read
-		 * 			and returned but kept back for the next line retrieval.
-		 * 
-		 * @return	A string containing the next line of text, or null if the end of
-		 * 			file has been reached.
-		 */
-		public String previewLine() throws java.io.IOException {
-			if( m_preview == null ) {
-				m_preview = readLine();
-			}
-			return m_preview;
-		}
-		
-		/**
-		 * @brief	Read the next line of text.
-		 * 
-		 * @return	A string containing the next line of text.
-		 * 
-		 * @throws java.io.IOException	Error while reading.
-		 */
-		private String readLine()  throws java.io.IOException {
-			String	line = m_in.readLine();
-			
-			if( line != null )
-			{
-				line = line.concat("\r\n");
-			}
-			return line;
-		}
-	};
-	
 	/**
 	 * @brief	The input object for the loader.
 	 */
-	private Reader m_in;
+	private Tokenizer m_tokenizer;
 	
 	/**
 	 * Constructor.
@@ -125,8 +49,8 @@ public class Loader {
 	 *  
 	 * @author gbrocker
 	 */
-	public Loader( java.io.InputStream input ) {
-		m_in = new Reader( input );
+	public Loader( InputStream input ) {
+		m_tokenizer = new Tokenizer( input );
 	}
 	
 	/**
@@ -136,22 +60,12 @@ public class Loader {
 	 * 
 	 * @author gbrocker
 	 */
-	public eclox.doxyfile.node.Doxyfile load() throws IOException {
-		eclox.doxyfile.node.Doxyfile	doxyfile;
-		Node							nextNode;
-		eclox.doxyfile.node.Group		curParent;
+	public Doxyfile load() throws IOException {
+		Doxyfile	doxyfile = new Doxyfile();
+		Group		currentParent = doxyfile;
 		
-		doxyfile = new eclox.doxyfile.node.Doxyfile(); 
-		curParent = doxyfile;
-		
-		for( nextNode = getNextNode(); nextNode != null; nextNode = getNextNode() ) {
-			if( nextNode.getClass() == Section.class ) {
-				doxyfile.addChild( nextNode );
-			}
-			else {
-				curParent.addChild( nextNode );
-			}
-		}
+		// Initialize the tokenizer.
+		//m_tokenizer.getToken();
 		
 		return doxyfile;
 	}
@@ -163,7 +77,7 @@ public class Loader {
 	 *
 	 * @throws java.io.IOException	Error while reading.
 	 */
-	private Node getNextNode() throws java.io.IOException {
+	/*private Node getNextNode() throws java.io.IOException {
 		String	nodeText = null;
 		Class	nodeClass = null;
 
@@ -207,14 +121,14 @@ public class Loader {
 			}
 		}
 		return getNodeFromClass( nodeClass, nodeText );
-	}
+	}*/
 	
 	/**
 	 * @brief	Retrieve the class of node corresponding to the specified line of text.
 	 * 
 	 * @return	The class of the node corresponding to the spcified line of text.
 	 */
-	private static Class getNodeClass( String line ) {	
+	/*private static Class getNodeClass( String line ) {	
 		String	head = line.substring( 0, 2 );
 		Class	nodeClass;
 		
@@ -231,7 +145,7 @@ public class Loader {
 			nodeClass = null;
 		}
 		return nodeClass;
-	}
+	}*/
 	
 	/**
 	 * @brief	Retrieve a node instance from the specified class and text.
@@ -239,7 +153,7 @@ public class Loader {
 	 * @param	nodeClass	A class of node to create.
 	 * @param	nodeText	The text of the node.
 	 */
-	private Node getNodeFromClass( Class nodeClass, String nodeText ) {
+	/*private Node getNodeFromClass( Class nodeClass, String nodeText ) {
 		Node	result;
 		
 		if( nodeClass == Section.class ) {
@@ -256,5 +170,5 @@ public class Loader {
 		}
 		
 		return result;
-	}
+	}*/
 }
