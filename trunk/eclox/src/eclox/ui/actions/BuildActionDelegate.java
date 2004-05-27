9c8ce4085b00f3648210c4cbad6909f85bcb5ecc
gbrocker <gbrocker@c73c6f0e-0c22-0410-8da1-b5b157a5ae3c>
2004-05-27 09:34:23 +0000

    [code] Creation of the default Doxygen action set. Generation of the empty Build action delegate. Creation of the 'icons' folder.

---
+++ trunk/eclox/src/eclox/ui/actions/BuildActionDelegate.java
@@ -0,0 +1,72 @@
+/*
+	eclox : Doxygen plugin for Eclipse.
+	Copyright (C) 2003 Guillaume Brocker
+
+	This file is part of eclox.
+
+	eclox is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	any later version.
+
+	eclox is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with eclox; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA	
+*/
+
+package eclox.ui.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.IWorkbenchWindowPulldownDelegate;
+
+/**
+ * Implement the action handling for the build action.
+ * 
+ * @author gbrocker
+ */
+public class BuildActionDelegate implements IWorkbenchWindowPulldownDelegate {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchWindowPulldownDelegate#getMenu(org.eclipse.swt.widgets.Control)
+	 */
+	public Menu getMenu(Control parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** (non-Javadoc)
+	 */
+	public void dispose() {
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IWorkbenchWindowActionDelegate#init(org.eclipse.ui.IWorkbenchWindow)
+	 */
+	public void init(IWorkbenchWindow window) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+	 */
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 */
+	public void selectionChanged(IAction action, ISelection selection) {
+	}
+
+}
