eb283ae2242024426c929959513f7c52dc1a469a
gbrocker <guillaume.brocker@gmail.com>
2006-02-05 18:04:03 +0000

    [code] In the editor, added support for directory path editor. Updated the editor type for following settings: HTML_OUTPUT, LATEX_OUTPUT, RTF_OUTPUT, MAN_OUTPUT, XML_OUTPUT and OUTPUT_DIRECTORY.

--- eclox/src/eclox/ui/editor/form/settings/editors/PathEditor.java
+++ eclox/src/eclox/ui/editor/form/settings/editors/PathEditor.java
@@ -19,18 +19,125 @@
 
 package eclox.ui.editor.form.settings.editors;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
+import org.eclipse.ui.dialogs.ContainerSelectionDialog;
 import org.eclipse.ui.forms.widgets.FormToolkit;
 
 public class PathEditor extends TextEditor {
+	
+	/**
+	 * the push button for browsing the file system
+	 */
+	private Button	browseFileSystemButton;
+	
+	/**
+	 * the push button for browsing eclipse's workspace
+	 */
+	private Button	browseWorkspaceButton;
+	
+	/**
+	 * Implements the selection listener attached to the push buttons.
+	 */
+	class MySelectionListener implements SelectionListener {
 
+		/**
+		 * @see org.eclipse.swt.events.SelectionListener#widgetDefaultSelected(org.eclipse.swt.events.SelectionEvent)
+		 */
+		public void widgetDefaultSelected(SelectionEvent e) {
+			if( e.widget == browseFileSystemButton ) {
+				browseFileSystem();
+			}
+			else if( e.widget == browseWorkspaceButton ) {
+				browseWorkspace();
+			}
+		}
+
+		/**
+		 * @see org.eclipse.swt.events.SelectionListener#widgetSelected(org.eclipse.swt.events.SelectionEvent)
+		 */
+		public void widgetSelected(SelectionEvent e) {
+			if( e.widget == browseFileSystemButton ) {
+				browseFileSystem();
+			}
+			else if( e.widget == browseWorkspaceButton ) {
+				browseWorkspace();
+			}
+		}
+		
+	}
+	
 	public void createContent(Composite parent, FormToolkit formToolkit) {
 		super.createContent(parent, formToolkit);
 		
-		// TODO Enhance the layout.
-		// TODO Add event handlers and associated treatements.
-		formToolkit.createButton( parent, "Browse Workspace", 0 );
-		formToolkit.createButton( parent, "Browse File System", 0 );
+		// Create controls and their associated layout data.
+		FormData				formData;
+		SelectionListener	selectionListener = new MySelectionListener();
+		
+		browseWorkspaceButton = formToolkit.createButton( parent, "Browse Workspace", 0 );
+		formData = new FormData();
+		formData.top = new FormAttachment( super.text, 0, SWT.BOTTOM );
+		formData.left = new FormAttachment( 0, 0 );
+		browseWorkspaceButton.setLayoutData( formData );
+		browseWorkspaceButton.addSelectionListener( selectionListener );
+		
+		browseFileSystemButton = formToolkit.createButton( parent, "Browse File System", 0 );
+		formData = new FormData();
+		formData.top = new FormAttachment( super.text, 0, SWT.BOTTOM );
+		formData.left = new FormAttachment( browseWorkspaceButton, 0, SWT.RIGHT );
+		browseFileSystemButton.setLayoutData( formData );
+		browseFileSystemButton.addSelectionListener( selectionListener );
 	}
 
+	/**
+	 * @see eclox.ui.editor.form.settings.editors.TextEditor#dispose()
+	 */
+	public void dispose() {
+		// Local cleaning.
+		browseFileSystemButton.dispose();
+		browseWorkspaceButton.dispose();
+		
+		// Default cleaning.
+		super.dispose();
+	}
+	
+	/**
+	 * Browses the workspace for a path and updates the managed text field.
+	 */
+	private void browseWorkspace() {
+		IWorkspaceRoot				root = ResourcesPlugin.getWorkspace().getRoot();
+		ContainerSelectionDialog		dialog = new ContainerSelectionDialog( super.text.getShell(), root, false, "Please, select a folder." );
+		
+		Object[]		result;
+		dialog.open();
+		result = dialog.getResult();
+		if( result != null && result.length >= 1 ) {
+			Path	 		path = (Path) result[0];
+			IContainer	container = (IContainer) root.findMember( path );
+			super.text.setText( container.getLocation().toOSString() );
+		}
+	}
+
+	/**
+	 * Browses the file system for a path and updates the managed text field.
+	 */
+	private void browseFileSystem() {
+		DirectoryDialog	dialog = new DirectoryDialog( super.text.getShell() );
+		String			path;
+		
+		path = dialog.open();
+		if( path != null ) {
+			super.text.setText( path );
+		}
+	}
 }
