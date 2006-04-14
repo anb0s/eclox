6e8234c4086d5278bb760e6a6183c59dd8dbf421
gbrocker <guillaume.brocker@gmail.com>
2006-04-14 17:06:40 +0000

    [code] Updated various icons (doxyfile, doxyfile wizard, build, ...)

--- eclox.ui/src/eclox/ui/Images.java
+++ eclox.ui/src/eclox/ui/Images.java
@@ -4,6 +4,8 @@ public interface Images {
 
 	static String CLEAR_CONSOLE = "icons/clear_console.gif";
 	
+	static String ERASE = "icons/erase.png";
+	
 	static String LOCK_CONSOLE = "icons/lock_console.gif";
 	
 	static String TERMINATE = "icons/terminate.gif";
