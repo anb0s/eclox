223c3727fff62f42356b0c391dac5db12d81092e
gbrocker <gbrocker@c73c6f0e-0c22-0410-8da1-b5b157a5ae3c>
2006-04-28 09:08:07 +0000

    [code] Reactivate the preference manage and edition. Work is still pending.

---
+++ trunk/eclox.ui/src/eclox/ui/IPreferences.java
@@ -0,0 +1,65 @@
+/*
+ * eclox : Doxygen plugin for Eclipse.
+ * Copyright (C) 2003-2006 Guillaume Brocker
+ *
+ * This file is part of eclox.
+ *
+ * eclox is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * eclox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with eclox; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA	
+ */
+
+package eclox.ui;
+
+/**
+ * Provides all preference names of the plugin.
+ *  
+ * @author gbrocker
+ */
+public interface IPreferences {
+	/**
+	 * Constant name for the doxygen binary's path.
+	 */
+//	public static final String COMPILER_PATH = "compiler-path";
+	
+	/**
+	 * Constant name for the build history content.
+	 */
+//	public static final String BUILD_HISTORY_CONTENT = "build-history-content";
+	
+	/**
+	 * Constant name for the build history size.
+	 */
+//	public static final String BUILD_HISTORY_SIZE = "build-history-size";
+	
+	/**
+	 * Constant name for workspace autosave flag.
+	 */
+	public static final String AUTO_SAVE = "editor.autosave";
+	
+	
+	/**
+	 * Constant for AUTO_SAVE never state.
+	 */
+	public static final String AUTO_SAVE_NEVER = "never";
+	
+	/**
+	 * Constant for AUTO_SAVE none always.
+	 */
+	public static final String AUTO_SAVE_ALWAYS = "always";
+	
+	/**
+	 * Constant for AUTO_SAVE none ask.
+	 */
+	public static final String AUTO_SAVE_ASK = "ask";
+}
---
+++ trunk/eclox.ui/src/eclox/ui/PreferencesInitializer.java
@@ -0,0 +1,46 @@
+/*
+ * eclox : Doxygen plugin for Eclipse.
+ * Copyright (C) 2003-2006 Guillaume Brocker
+ *
+ * This file is part of eclox.
+ *
+ * eclox is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * any later version.
+ *
+ * eclox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with eclox; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA	
+ */
+
+package eclox.ui;
+
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+
+/**
+ * Implements the prefenrence initializer for the plugin.
+ * 
+ * @author gbrocker
+ */
+public class PreferencesInitializer extends AbstractPreferenceInitializer {
+
+	/**
+	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#initializeDefaultPreferences()
+	 */
+	public void initializeDefaultPreferences() {
+		IPreferenceStore	preferences = Plugin.getDefault().getPreferenceStore();
+		
+//		preferences.setDefault( IPreferences.BUILD_HISTORY_CONTENT, "" );
+//		preferences.setDefault( IPreferences.BUILD_HISTORY_SIZE, 4 );
+		preferences.setDefault( IPreferences.AUTO_SAVE, IPreferences.AUTO_SAVE_ASK );
+	}
+
+}
