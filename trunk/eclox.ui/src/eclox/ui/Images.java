256c36ef62c4134f54c06dcbd7cb964f998fe90d
gbrocker <gbrocker@c73c6f0e-0c22-0410-8da1-b5b157a5ae3c>
2007-03-02 14:59:59 +0000

    task #3126 : Enhance the doxyfile creation wizard. Initialize the new doxyfile name with the enclosing eclipse project.

--- trunk/eclox.ui/src/eclox/ui/Images.java
+++ trunk/eclox.ui/src/eclox/ui/Images.java
@@ -2,12 +2,10 @@ package eclox.ui;
 
 public interface Images {
 
-	static String CLEAR_CONSOLE = "icons/clear_console.gif";
-	
-	static String ERASE = "icons/erase.png";
-	
-	static String LOCK_CONSOLE = "icons/lock_console.gif";
-	
-	static String TERMINATE = "icons/terminate.gif";
+	static String DOXYFILE_WIZARD	= "icons/doxyfile_wizard_big.png";
+	static String CLEAR_CONSOLE		= "icons/clear_console.gif";
+	static String ERASE				= "icons/erase.png";
+	static String LOCK_CONSOLE		= "icons/lock_console.gif";
+	static String TERMINATE			= "icons/terminate.gif";
 	
 }
