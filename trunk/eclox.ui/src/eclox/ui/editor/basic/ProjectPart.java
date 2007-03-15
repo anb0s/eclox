77f8dbfa78050b9af1bfae9cd845a65a8ebbf79c
gbrocker <gbrocker@c73c6f0e-0c22-0410-8da1-b5b157a5ae3c>
2007-03-15 16:27:43 +0000

    Splits the page content creation into parts.

---
+++ trunk/eclox.ui/src/eclox/ui/editor/basic/ProjectPart.java
@@ -0,0 +1,46 @@
+// eclox : Doxygen plugin for Eclipse.
+// Copyright (C) 2003-2007 Guillaume Brocker
+//
+// This file is part of eclox.
+//
+// eclox is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// any later version.
+//
+// eclox is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with eclox; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA	
+
+package eclox.ui.editor.basic;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.forms.SectionPart;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Section;
+
+public class ProjectPart extends SectionPart {
+
+	ProjectPart( Composite parent, FormToolkit toolkit ) {
+		super( parent, toolkit, Section.TITLE_BAR );
+		
+		Section		section	= getSection();
+		Composite	content	= toolkit.createComposite(section);
+		
+		section.setText("Project");
+		section.setClient(content);
+		
+		toolkit.createLabel(content, "Provide some documentation about the project you are documenting.", SWT.WRAP);
+
+		content.setLayout( new FillLayout(SWT.VERTICAL) );
+		
+				
+	}
+}
