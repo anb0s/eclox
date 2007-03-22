03b58f9fd2a725dd6b4669c3d3fc5ea134a16acc
gbrocker <gbrocker@c73c6f0e-0c22-0410-8da1-b5b157a5ae3c>
2007-03-22 11:54:34 +0000

    Update the icon of the wizard for new doxyfiles.

--- trunk/eclox.ui/src/eclox/ui/Images.java
+++ trunk/eclox.ui/src/eclox/ui/Images.java
@@ -2,7 +2,7 @@ package eclox.ui;
 
 public interface Images {
 
-	static String DOXYFILE_WIZARD	= "icons/doxyfile_wizard_big.png";
+	static String DOXYFILE_WIZARD	= "icons/doxyfile_wiz.gif";
 	static String CLEAR_CONSOLE		= "icons/clear_console.gif";
 	static String ERASE				= "icons/erase.png";
 	static String LOCK_CONSOLE		= "icons/lock_console.gif";
