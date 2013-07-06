2eccc0de50cd4aac52131cf2db1c86621ebca393
gbrocker <gbrocker@c73c6f0e-0c22-0410-8da1-b5b157a5ae3c>
2013-07-06 13:01:26 +0000

    Change GPL V2 license for EPL 1.0 to allow debian packaging.

--- trunk/eclox.core/src/eclox/core/doxygen/CustomDoxygen.java
+++ trunk/eclox.core/src/eclox/core/doxygen/CustomDoxygen.java
@@ -1,23 +1,15 @@
-/*
- * eclox : Doxygen plugin for Eclipse.
- * Copyright (C) 2003-2006 Guillaume Brocker
+/*******************************************************************************
+ * Copyright (C) 2003-2006, 2013, Guillaume Brocker
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
- * This file is part of eclox.
+ * Contributors:
+ *     Guillaume Brocker - Initial API and implementation
  *
- * eclox is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * any later version.
- *
- * eclox is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with eclox; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA	
- */
+ ******************************************************************************/ 
 
 package eclox.core.doxygen;
 
--- trunk/eclox.ui/src/eclox/ui/editor/Location.java
+++ trunk/eclox.ui/src/eclox/ui/editor/Location.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (C) 2003-2013, Guillaume Brocker
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Guillaume Brocker - Initial API and implementation
+ *
+ ******************************************************************************/ 
+
 package eclox.ui.editor;
 
 import org.eclipse.ui.IMemento;
