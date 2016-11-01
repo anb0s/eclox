4c2dd03aee3e09eebe9f4d6d7601be51ee214fa6
Andre Bossert <anb0s@anbos.de>
2016-11-01 20:27:36 +0100

    added variable selection button for doxygen path
    Signed-off-by: Andre Bossert <anb0s@anbos.de>

--- eclox.core/src/eclox/core/ui/CustomDoxygenDialog.java
+++ eclox.core/src/eclox/core/ui/CustomDoxygenDialog.java
@@ -13,10 +13,20 @@
 
 package eclox.core.ui;
 
+import java.io.File;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.variables.IStringVariableManager;
+import org.eclipse.core.variables.VariablesPlugin;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.preference.DirectoryFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
@@ -48,7 +58,73 @@ public class CustomDoxygenDialog extends Dialog {
 			@Override
 			protected void createFieldEditors() {
 				targetDirectoryEditor = new DirectoryFieldEditor("",
-						"Custom Doxygen directory:", getFieldEditorParent());
+						"Custom Doxygen directory:", getFieldEditorParent()) {
+		            /** The own control is the variableButton */
+		            private static final int NUMBER_OF_OWN_CONTROLS = 1;
+
+		            @Override
+		            protected boolean doCheckState() {
+		                String dirName = getTextControl().getText();
+		                dirName = dirName.trim();
+		                if (dirName.length() == 0 && isEmptyStringAllowed())
+		                    return true;
+		                IStringVariableManager manager = VariablesPlugin.getDefault()
+		                        .getStringVariableManager();
+		                String substitutedDirName;
+		                try {
+		                    substitutedDirName = manager
+		                            .performStringSubstitution(dirName);
+		                } catch (CoreException e) {
+		                    // It's apparently invalid
+		                    return false;
+		                }
+		                File dir = new File(substitutedDirName);
+		                // require the file to exist
+		                return dir.exists() && dir.isDirectory();
+		            }
+
+		            @Override
+		            public int getNumberOfControls() {
+		                return super.getNumberOfControls() + NUMBER_OF_OWN_CONTROLS;
+		            }
+
+		            @Override
+		            protected void doFillIntoGrid(Composite parent, int numColumns) {
+		                super.doFillIntoGrid(parent,
+		                        numColumns - NUMBER_OF_OWN_CONTROLS);
+		            }
+
+		            @Override
+		            protected void adjustForNumColumns(int numColumns) {
+		                super.adjustForNumColumns(numColumns - NUMBER_OF_OWN_CONTROLS);
+		            }
+
+		            @Override
+		            protected void createControl(Composite parent) {
+		                // setting validate strategy using the setter method is too late
+		                super.setValidateStrategy(
+		                        StringFieldEditor.VALIDATE_ON_KEY_STROKE);
+		                super.createControl(parent);
+		                if (hasDebugUiBundle()) {
+		                    addVariablesButton(parent);
+		                }
+		            }
+
+		            private void addVariablesButton(Composite parent) {
+		                Button variableButton = new Button(parent, SWT.PUSH);
+		                variableButton.setText("&Variable...");
+		                variableButton.addSelectionListener(new SelectionAdapter() {
+		                    @Override
+		                    public void widgetSelected(SelectionEvent e) {
+		                        org.eclipse.debug.ui.StringVariableSelectionDialog dialog = new org.eclipse.debug.ui.StringVariableSelectionDialog(
+		                                getShell());
+		                        int returnCode = dialog.open();
+		                        if (returnCode == Window.OK)
+		                            setStringValue(dialog.getVariableExpression());
+		                    }
+		                });
+		            }
+				};
 				targetDirectoryEditor.setStringValue(targetDirectory == null ? "" : targetDirectory);
 				addField(targetDirectoryEditor);
 			}
@@ -94,4 +170,14 @@ public class CustomDoxygenDialog extends Dialog {
 	public String getTargetDirectory() {
 		return targetDirectory;
 	}
+
+    private static final boolean hasDebugUiBundle() {
+        try {
+            return Class
+                    .forName("org.eclipse.debug.ui.StringVariableSelectionDialog") != null; //$NON-NLS-1$
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
 }
